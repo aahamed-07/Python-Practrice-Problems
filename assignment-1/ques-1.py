e401b8d33926bd024dbe55de1811182c5581ac49
ALi <ahamed.aaa01@gmail.com>
2024-06-13 17:36:54 +0530

    more parts of ques.1 added till part 10

--- assignment-1/ques-1.py
+++ assignment-1/ques-1.py
@@ -13,7 +13,6 @@ L= [11, 12, 13, 14]
 del L[0]                #indices have shifted after first deletion
 del L[1]
 print("List after removing 11,13: ",L)
-
 #iii.WAP to sort L in ascending order
 L= [13, 12, 14, 11]         #Random List  
 L.sort()
@@ -23,3 +22,41 @@ print("Sorted List: ",L)
 L= [13, 12, 14, 11]         #Random List  
 L.sort(reverse=True)
 print("Sorted List Descending order: ",L)
+
+#v.WAP to search for 13 in L
+L= [14, 13, 12, 11] 
+print(L.index(13))
+
+#vi.WAP to count the number of elements present in L
+print(len(L))
+
+#vii.WAP to sum all the elements in L
+sum = sum(L)
+print(sum)
+
+#viii.WAP to sum all ODD numbers in L
+sum_odd = 0
+
+for i in L:
+    if i%2 != 0:
+        sum_odd+=i
+
+print(sum_odd)
+
+#ix.WAP to sum all EVEN numbers in L
+sum_even = 0
+
+for i in L:
+    if i%2 == 0:
+        sum_even+=i
+
+print(sum_even)
+
+#x.WAP to sum all PRIME numbers in L
+sum_prime = 0
+
+for i in L:
+    if i != 0:
+        sum_odd+=i
+
+print(sum_odd)
